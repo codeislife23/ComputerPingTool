07d1a0a86f792f230d6a238de10cfdd5e36a2258
codeislife23 <sjadelakun@gmail.com>
2023-08-04 13:53:32 -0500

    Add files via upload

---
+++ Check Computer Availability.ps1
@@ -0,0 +1,12 @@
+﻿$computers = Get-Content -Path "C:\temp\computerlist.txt"
+foreach ($computer in $computers)
+     {
+     $ip = $computer.Split(" - ")[0]
+     if (Test-Connection  $ip -Count 1 -ErrorAction SilentlyContinue){
+         Write-Host "$ip is up"
+         }
+     else{
+         Write-Host "$ip is down"
+         }
+     }
+Read-Host -Prompt "Press Enter to exit"
